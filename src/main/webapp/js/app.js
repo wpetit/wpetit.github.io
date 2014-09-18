cd5abf9754eea47ca632c01a82cda01de803094d
wpetit <petit.wilfried@gmail.com>
2014-09-18 20:49:21 +0200

    Update UI

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,3 +1,20 @@
-var app = angular.module('wpetitApp', [ 'ngRoute', 'ui.bootstrap', 'toaster', 'wpetitAppControllers' ]);
+// FIX for carousel see : https://github.com/angular-ui/bootstrap/issues/1350
+angular.module('ui.bootstrap.setNgAnimate', [ 'ngAnimate' ]).directive(
+		'setNgAnimate', [ '$animate', function($animate) {
+			return {
+				link : function($scope, $element, $attrs) {
+
+					$scope.$watch(function() {
+						return $scope.$eval($attrs.setNgAnimate, $scope);
+					}, function(valnew, valold) {
+						$animate.enabled(!!valnew, $element);
+					});
+
+				}
+			};
+		} ]);
+
+var app = angular.module('wpetitApp', [ 'ui.bootstrap', 'ngAnimate', 'ngTouch',
+		'ui.bootstrap.setNgAnimate', 'wpetitAppControllers' ]);
 
 var wpetitAppControllers = angular.module('wpetitAppControllers', []);
--- src/main/webapp/js/controller/controller.js
+++ src/main/webapp/js/controller/controller.js
@@ -1,12 +1,22 @@
 // Create a controller with name wpetitCtrl to bind to the html page.
-wpetitAppControllers.controller('wpetitCtrl', function($scope, $location,
-		$routeParams, $modal, toaster, applicationService) {
-	
+wpetitAppControllers.controller('wpetitCtrl', function($scope,
+		applicationService) {
+
 	$scope.loadApplications = function() {
 		applicationService.getApplications().success(function(data) {
 			$scope.applications = data.applications;
 		});
-	}
-	
+	};
+
 	$scope.loadApplications();
+
+	$scope.$watch('applications', function(newVal) {
+		$scope.currentApp = null;
+		for (app in $scope.applications) {
+			if (app.active) {
+				$scope.currentApp = app;
+				break;
+			}
+		}
+	});
 });
